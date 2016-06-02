0db883c8ecfc83f6e8469566de1eb8ef078392e0
Jiar <e@mail.email>
2016-06-02 14:41:06 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "furry-octo-giggle"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/Core/Main.scala
@@ -0,0 +1,13 @@
+package Core
+
+/**
+  * Created by jiar on 2-6-16.
+  */
+object Main extends App {
+  welcome()
+
+  def welcome(): Unit = {
+    print("welcometogigglingoctofurries")
+    welcome()
+  }
+}
